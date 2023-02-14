1acb49bc02e00ebc2f25862b442c248ba2a358ba
wishba <wisnubayu34@gmail.com>
2023-02-14 09:59:30 +0700

    add input durasi per hari

--- TugasPV/Form1.Designer.vb
+++ TugasPV/Form1.Designer.vb
@@ -23,22 +23,64 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Location = New System.Drawing.Point(62, 54)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(81, 16)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Hello World!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Total :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Durasi Per Hari : "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(125, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Pesan"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -48,4 +90,8 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- TugasPV/Form1.vb
+++ TugasPV/Form1.vb
@@ -1,12 +1,14 @@
 ﻿Imports System.IO
 
 Public Class Form1
+    Private hargaPerHari As Integer
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim fileName As String = "data.txt"
         Dim fileContents As String = File.ReadAllText(fileName)
         Dim lines As String() = fileContents.Split(Environment.NewLine)
 
-        Dim hargaPerHari As Integer = 0
+        'Dim hargaPerHari As Integer = 0
         For Each line As String In lines
             If line.StartsWith("hargaPerHari=") Then
                 hargaPerHari = Integer.Parse(line.Split("=")(1))
@@ -14,10 +16,16 @@ Public Class Form1
             End If
         Next
 
-        Label1.Text = hargaPerHari.ToString()
+        'Label1.Text = hargaPerHari.ToString()
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim multiplier As Integer = Integer.Parse(TextBox1.Text)
+        Dim result As Integer = hargaPerHari * multiplier
+        Label1.Text = result.ToString()
     End Sub
 End Class
