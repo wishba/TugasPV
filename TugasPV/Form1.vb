d96c34c042e0e9f9d771697b727df85d5f3cba14
wishba <wisnubayu34@gmail.com>
2023-02-14 09:31:16 +0700

    display harga

--- TugasPV/Form1.vb
+++ TugasPV/Form1.vb
@@ -1,6 +1,20 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fileName As String = "data.txt"
+        Dim fileContents As String = File.ReadAllText(fileName)
+        Dim lines As String() = fileContents.Split(Environment.NewLine)
+
+        Dim hargaPerHari As Integer = 0
+        For Each line As String In lines
+            If line.StartsWith("hargaPerHari=") Then
+                hargaPerHari = Integer.Parse(line.Split("=")(1))
+                Exit For
+            End If
+        Next
+
+        Label1.Text = hargaPerHari.ToString()
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
